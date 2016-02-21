89b2abae1f660e625c5bb51148c95150b92335ef
James Burgess <jamesmillerburgess@gmail.com>
2016-02-21 07:57:58 +0100

    Enforce post URL uniqueness.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -12,7 +12,10 @@ Template.postSubmit.events({
       if (error)
         return alert(error.reason);
 
-      Router.go('postPage', {_id:result._id});
+      if (result.postExists)
+        alert('This link has already been posted');
+
+      Router.go('postPage', {_id: result._id});
     });
 
 
