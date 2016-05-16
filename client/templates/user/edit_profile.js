d1b01326f4c4fff5279ce337b599a44bda16b98f
James Burgess <jamesmillerburgess@gmail.com>
2016-05-16 03:24:36 +0200

    Ability to save native language and language of study.

--- client/templates/user/edit_profile.js
+++ client/templates/user/edit_profile.js
@@ -6,3 +6,29 @@ Template.editProfile.onRendered(function () {
 Template.editProfile.helpers({
   username: () => Meteor.user().username,
 });
+
+Template.editProfile.events({
+  'submit form': e => {
+    e.preventDefault();
+
+
+    let profileProperties = {
+      nativeLanguage: $(e.target).find('[name=nativeLanguage]').val(),
+      languageOfStudy: $(e.target).find('[name=languageOfStudy]').val(),
+    };
+
+    // TODO: Write validateProfile function
+    // let errors = validateProfile(profileProperties);
+    //if (errors.nativeLanguage || errors.languageOfStudy)
+    //  return Session.set('postEditErrors', errors);
+
+    Meteor.users.update(Meteor.user()._id, {$set: { profile: profileProperties } }, function (error) {
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
+      } else {
+        //Router.go('postPage', {_id: currentPostId});
+      }
+    });
+  }
+});
