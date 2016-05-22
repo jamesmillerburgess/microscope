1139d40381257bc557ad3370076eb5d2b7704829
James Burgess <jamesmillerburgess@gmail.com>
2016-05-22 22:08:41 +0200

    Added default languages to new profiles.

--- server/accounts.js
+++ server/accounts.js
@@ -1,5 +1,7 @@
 Accounts.onCreateUser(function(options, user) {
   if (options.profile) {
+    options.profile.nativeLanguage = 'English';
+    options.profile.languageOfStudy = 'English';
     options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
     user.profile = options.profile;
   }
