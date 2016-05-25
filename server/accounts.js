c126e9b8f7b0200723e07eee878b0ba85e41fe73
JBurgess <jamesmillerburgess@gmail.com>
2016-05-25 07:59:11 +0200

    Fixed profile pic error when creating account with accounts-password.

--- server/accounts.js
+++ server/accounts.js
@@ -1,8 +1,10 @@
-Accounts.onCreateUser(function(options, user) {
+Accounts.onCreateUser(function (options, user) {
   if (options.profile) {
     options.profile.nativeLanguage = 'English';
     options.profile.languageOfStudy = 'English';
-    options.profile.picture = "https://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+    if (user.services.facebook) {
+      options.profile.picture = "https://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+    }
     user.profile = options.profile;
   }
   return user;
